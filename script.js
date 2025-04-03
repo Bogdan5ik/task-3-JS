0f36a9483a487a18daccbb1c16a86822eb933a55
Bogdan5ik <baggi.juk@mail.ru>
2025-04-03 20:52:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector('.btn').onclick = function() {
+    document.querySelector('.gif').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(2)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(3)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(4)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(5)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(6)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(7)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(8)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(9)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(10)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(11)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(12)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(13)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(14)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(15)').style.top = "95%"
+
+    document.querySelector('.gif:nth-child(16)').style.top = "95%"
+
+}
