0b6bee60cbba62df261c9f143f5234973a4b2452
Bogdan5ik <baggi.juk@mail.ru>
2025-04-05 11:20:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 document.querySelector('.btn').onclick = function() {
     document.querySelector('.gif').style.top = "95%"
+    document.querySelector('.gif').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(2)').style.top = "95%"
+    document.querySelector('.gif:nth-child(2)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(3)').style.top = "95%"
+    document.querySelector('.gif:nth-child(3)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(4)').style.top = "95%"
+    document.querySelector('.gif:nth-child(4)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(5)').style.top = "95%"
+    document.querySelector('.gif:nth-child(5)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(6)').style.top = "95%"
+    document.querySelector('.gif:nth-child(6)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(7)').style.top = "95%"
+    document.querySelector('.gif:nth-child(7)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(8)').style.top = "95%"
+    document.querySelector('.gif:nth-child(8)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(9)').style.top = "95%"
+    document.querySelector('.gif:nth-child(9)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(10)').style.top = "95%"
+    document.querySelector('.gif:nth-child(10)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(11)').style.top = "95%"
+    document.querySelector('.gif:nth-child(11)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(12)').style.top = "95%"
+    document.querySelector('.gif:nth-child(12)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(13)').style.top = "95%"
+    document.querySelector('.gif:nth-child(13)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(14)').style.top = "95%"
+    document.querySelector('.gif:nth-child(14)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(15)').style.top = "95%"
+    document.querySelector('.gif:nth-child(15)').style.visible = "hidden"
 
     document.querySelector('.gif:nth-child(16)').style.top = "95%"
+    document.querySelector('.gif').style.visible = "hidden"
 
 }
